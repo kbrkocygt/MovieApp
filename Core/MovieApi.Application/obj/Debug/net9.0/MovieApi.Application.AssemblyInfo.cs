0f380d91b095007894ee21fbcb71a1d0c60460ea
kbrkocygt <kcygt.kbra@gmail.com>
2026-02-28 00:26:29 +0300

    Cast class with mediator

--- Core/MovieApi.Application/obj/Debug/net9.0/MovieApi.Application.AssemblyInfo.cs
+++ Core/MovieApi.Application/obj/Debug/net9.0/MovieApi.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c9e4639ed570fb88971319e2ee7b715c099d91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f1f1de96c098a0cab80a47062fa29a69f3cb3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
